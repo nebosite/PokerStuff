819e6d333930f622300087bca1c431a560ba23c0
Eric Jorgensen <erjorge@microsoft.com>
2019-05-15 08:04:57 -0700

    Load odds tables at program start

--- OddsMaster/WpfClient/Models/AppModel.cs
+++ OddsMaster/WpfClient/Models/AppModel.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +14,16 @@ namespace OddsMaster
     {
         public FlashGameModel FlashGame { get; set; } = new FlashGameModel();
         public TableGenModel TableGen { get; set; } = new TableGenModel();
+        public static Dictionary<int, Dictionary<string, double>> PocketHandOdds { get; private set; }
 
-
+        static AppModel()
+        {
+            var resourceName = Assembly.GetExecutingAssembly().GetManifestResourceNames().Where(n => n.Contains("PocketHandOdds")).FirstOrDefault();
+            using (var oddsStream = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)))
+            {
+                var oddsJson = oddsStream.ReadToEnd();
+                PocketHandOdds = JsonConvert.DeserializeObject<Dictionary<int, Dictionary<string, double>>>(oddsJson);
+            }
+        }
     }
 }
